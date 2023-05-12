f104313c7bad7e2d527fc8be39d26969966205c7
Gerson Pineda <pinedagerson1993@gmail.com>
2023-05-11 21:59:27 -0400

    Arreglos en estructura de carpetas

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,11 @@
 // import logo from './platzi.webp';
 
-import { TodoCounter } from './TodoCounter';
-import { TodoSearch } from './TodoSearch';
-import { TodoList } from './TodoList';
-import { TodoItem } from './TodoItem';
-import { TodoCreateButton } from './TodoCreateButton';
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
+import { TodoCreateButton } from '../TodoCreateButton';
+import { useLocalStorage } from './useLocalStorage';
 
 import React from 'react';
 
@@ -18,23 +19,10 @@ import React from 'react';
 // localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
 // localStorage.removeItem('TODOS_V1');
 
-
 function App() {
 
-  const localStorageTodos = localStorage.getItem('TODOS_V1');
 
-  let parsedTodos;
-
-  if (!localStorageTodos) {
-    localStorage.setItem('TODOS_V1', JSON.stringify([]));
-    parsedTodos = [];
-  } else {
-    //localStorage.setItem('TODOS_V1', JSON.stringify([]));
-    parsedTodos = JSON.parse(localStorageTodos);
-  }
-
-
-  const [todos, setTodos] = React.useState(parsedTodos);
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter((todo) => !!todo.completed).length;
@@ -46,16 +34,11 @@ function App() {
     return todoText.includes(searchText);
   });
 
-  const saveTodos = (newTodos) => {
-    localStorage.setItem('TODOS_V1', JSON.stringify(newTodos));
-    setTodos(newTodos);
-  };
-
   const completeTodo = (todoIndex) => {
     const newTodos = [...todos];
     // Colocar text como parámetro
     // const todoIndex = newTodos.findIndex(
-    //   (todo) => todo.text == text
+    //   (todo) => todo.text === text
     // );
     newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
     saveTodos(newTodos);
